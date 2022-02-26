ad0a96b40851ffe2f8b7b92b0e2442309ccc7b2b
virajsharma2000 <kaushik.kalpana@gmail.com>
2022-02-26 22:41:23 +0530

    my buges joke teller python app

---
+++ bugjokes.py
@@ -0,0 +1,8 @@
+import random
+a = 'I am a debugger but 1 day I could not debug feature bug.'
+b = "if you won't write code you will not get bugs."
+c = 'other people get good things in gift but programmers get bugs in gift.'
+d = 'I wrote a code I got a bug I could not fix it.One day I got anathor bug I fixed it but I could not fix bug which I got before that bug which I fixed it.'
+jokelist = [a,b,c,d]
+x = random.choice(jokelist)
+print(x)
