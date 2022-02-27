5aec18fb1f6526d685176cbdbe24c0c0d6bc6b65
virajsharma2000 <86789278+virajsharma2000@users.noreply.github.com>
2022-02-27 10:01:35 +0530

    Delete account.py

--- account.py
+++
@@ -1,6 +0,0 @@
-import mysql.connector
-x = mysql.connector.connect(host = 'localhost',
-                               username = 'viraj',
-                               password =  'sampulili'
-                        )
-print(x)
