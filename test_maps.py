ffb7616eb65a03c3179f58007d8bdaae06f710e3
virajsharma2000 <kaushik.kalpana@gmail.com>
2022-05-28 10:25:21 +0530

    adding test file of module called maps

---
+++ test_maps.py
@@ -0,0 +1,27 @@
+# importing important libary
+
+import maps
+
+# creating map using maps module
+
+maps.createmap(
+
+               # my google map
+    
+
+              'my Google map',
+
+               # adding latitude of map
+
+               '21.125498',
+
+               # adding longetude
+
+               '81.914063',
+
+               # zooming the map in level
+
+               '15'
+
+               )
+
