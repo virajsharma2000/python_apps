b40bbcb4d94d47aecd805b577ca1dd00c6f99aaa
Viraj Sharma <virajsharma1501@gmail.com>
2022-10-16 11:16:17 +0530

    making more good changes in reminders app

--- reminders/reminder.py
+++ reminders/reminder.py
@@ -15,18 +15,19 @@ while True:
 
  c = mydb.cursor()
 
- c.execute('select reminder_id, reminder_status, remind_for,  reminder_time_hours, reminder_time_minutes, reminder_time_am_or_pm from reminders')
+ c.execute('select * from reminders')
 
  r = c.fetchall()
  for records in r:
    
-   if int(time.strftime('%I')) == records[3] and int(time.strftime('%M')) == records[4] and time.strftime('%P') == records[5]:
-        status = record[1]
+   if int(time.strftime('%I')) == records[2] and int(time.strftime('%M')) == records[3] and time.strftime('%P') == records[4]:
+        status = records[5]
+        sound_to_play = records[6]
 
         if status == 'pending':
-         remind_for = records[2]
+         remind_for = records[1]
          plyer.notification.notify(title = 'Reminder',message = remind_for)
-         playsound.playsound('alarmsiren.mp3')
+         playsound.playsound(sound_to_play)
 
          c.execute('UPDATE reminder SET reminder_status="completed" WHERE reminder_id={}'.format(record[0]))
          mydb.commit()
@@ -38,14 +39,14 @@ while True:
             continue
 
         if status == 'repeated':
-            remind_for = records[2]
+            remind_for = records[1]
             plyer.notification.notify(title = 'Reminder',message = remind_for)
             playsound.playsound('alarmsiren.mp3')
 
    else:
       continue
 
- time.sleep(60)
+   time.sleep(60)
       
 
  
