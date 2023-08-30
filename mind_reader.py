63870105c3478d0735166cd7d1912a4e0aa72c53
Viraj Sharma <virajsharma1501@gmail.com>
2023-08-30 22:34:19 +0530

    my python library to read people's thought number

---
+++ mind_reader.py
@@ -0,0 +1,12 @@
+# ensuring that this file runs by importing only
+
+if __name__ == 'mind_reader':
+ # in this library first people are asked to think of a number then add it by the same number which the person thought then put it in mind_reader's read_mind function
+
+ print('think of a number then add the number by itself')
+
+ # this function gets the mind thought number by answer of calculation mind thought number + mind thought number
+
+ def read_mind(answer_of_calculations):
+    return answer_of_calculations // 2
+
---
+++ mind_reader_test.py
@@ -0,0 +1,7 @@
+import mind_reader
+
+answer_of_calculations = int(input('Enter the answer of calculations given above: '))
+
+number = mind_reader.read_mind(answer_of_calculations)
+
+print(number)
