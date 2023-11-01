f117b8eac93f43436e0ef98657ffafae0a9bf6a7
Viraj Sharma <virajsharma1501@gmail.com>
2023-11-01 14:39:14 +0530

    improove in metro_route app

--- metro_route.py
+++ metro_route.py
@@ -12,7 +12,7 @@ def get_metro_route(source,destination):
    metro_station = station_list[array_index]
 
    if metro_station == destination:
-    metro_route += metro_station 
+    metro_route += metro_station
 
    else:
     metro_route += metro_station + ','
@@ -33,16 +33,29 @@ def get_metro_route(source,destination):
 
  return metro_route
 
+def get_tag_by_metro_station(tag):
+ metro_stations_and_tags = open('metro_stations_and_tags','r').read()
 
-source_metro_station = input('Enter onboarding metro station from above choices: ')
-source_tag = input('Enter onboarding metro station tag from above choices: ')
+ for metro_stations in metro_stations_and_tags.split('\n'):
+    if metro_stations.split(' - ')[0] == tag:
+        metro_station = metro_stations.split(' - ')[1]
 
+        return metro_station
+
+    else:
+        continue
+    
+
+source_metro_station = input('Enter onboarding metro station: ')
 destination_metro_station = input('Enter destination metro station: ')
-destination_tag = input('Enter destination tag: ')
 
-route = get_metro_route('{} - {}'.format(source_metro_station,source_tag),'{} - {}'.format(destination_metro_station,destination_tag))
+source_tag = input('Enter onboarding metro station tag: ')
+destination_tag = input('Enter destination metro station tag: ')
 
-print(route)
+route = get_metro_route(source_metro_station,destination_metro_station)
 
-  
+if source_tag == get_tag_by_metro_station(source_metro_station) and destination_tag == get_tag_by_metro_station(destination_metro_station):
+    print(route)
 
+else:
+    print('I found the route but I will not show you the route I found because the entered tag of destination metro station or source metro station is incorrect')
