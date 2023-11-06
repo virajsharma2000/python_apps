92d3a6940b28fde17daab2bc785778ad1770ed69
Viraj Sharma <virajsharma1501@gmail.com>
2023-11-06 21:06:39 +0530

    an app for fake hypnotism

---
+++ hypnotizer.py
@@ -0,0 +1,9 @@
+import webview
+
+def hypnotize():
+    webview.create_window('hypnotizer','https://i.pinimg.com/originals/de/0e/70/de0e706fbc5c69ebc13ff52366cf7f7d.gif')
+    
+    webview.start()
+    
+
+hypnotize()
