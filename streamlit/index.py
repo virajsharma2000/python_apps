e48475a6378d2e19f9fbb1f7780ffd7a4a05f01e
Viraj Sharma <virajsharma1501@gmail.com>
2023-11-17 19:16:42 +0530

    streamlit first app

---
+++ streamlit/index.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
+
+st.title("this is my strealit test")
+
+title = st.text_input('Movie title', 'Life of Brian')
+st.write('The current movie title is', title)
