7e9ea659b0883c27032b8e5fe4b627f1d7bdb584
Viraj Sharma <virajsharma1501@gmail.com>
2023-12-23 14:39:44 +0530

    updates in my eye test app

--- streamlit/eye_test.py
+++ streamlit/eye_test.py
@@ -11,20 +11,30 @@ def generate_random_letters():
 
  return random_letters
 
+streamlit.write('stand 20 feet away from this chart \n\n')
 
-streamlit.write('<p style = "font-size:18px;">1.{}</p>'.format(generate_random_letters()),unsafe_allow_html = True)
-streamlit.write('<p style = "font-size:16px;">2.{}</p>'.format(generate_random_letters()),unsafe_allow_html = True)
-streamlit.write('<p style = "font-size:14px;">3.{}</p>'.format(generate_random_letters()),unsafe_allow_html = True)
-streamlit.write('<p style = "font-size:12px;">4.{}</p>'.format(generate_random_letters()),unsafe_allow_html = True)
-streamlit.write('<p style = "font-size:10px;">5.{}</p>'.format(generate_random_letters()),unsafe_allow_html = True)
+streamlit.write('<p style = "font-size:152px;">1.{}</p>'.format(generate_random_letters()),unsafe_allow_html = True)
+streamlit.write('<p style = "font-size:130px;">2.{}</p>'.format(generate_random_letters()),unsafe_allow_html = True)
+streamlit.write('<p style = "font-size:108px;">3.{}</p>'.format(generate_random_letters()),unsafe_allow_html = True)
+streamlit.write('<p style = "font-size:87px;">4.{}</p>'.format(generate_random_letters()),unsafe_allow_html = True)
+streamlit.write('<p style = "font-size:65px;">6.{}</p>'.format(generate_random_letters()),unsafe_allow_html = True)
+streamlit.write('<p style = "font-size:48px;">7.{}</p>'.format(generate_random_letters()),unsafe_allow_html = True)
+streamlit.write('<p style = "font-size:33px;">8.{}</p>'.format(generate_random_letters()),unsafe_allow_html = True)
+streamlit.write('<p style = "font-size:21px;">9.{}</p>'.format(generate_random_letters()),unsafe_allow_html = True)
+streamlit.write('<p style = "font-size:15px;">10.{}</p>'.format(generate_random_letters()),unsafe_allow_html = True)
+streamlit.write('<p style = "font-size:9px;">11.{}</p>'.format(generate_random_letters()),unsafe_allow_html = True)
 
+line_numbers_and_their_font_sizes = {'1':152,'2':130,'3':108,'4':87,'6':65,'7':48,'8':33,'9':21,'10':15,'11':9}
+                   
 last_line_number_able_to_see = streamlit.text_input("which last line number you are able to see")
 
-if streamlit.button('get font size'):
- if not int(last_line_number_able_to_see) > 6:
-  font_size = 18 - (int(last_line_number_able_to_see) - 1) * 2
+if streamlit.button('eye number'):
+ if not int(last_line_number_able_to_see) > 11:
+  eye_number = int(line_numbers_and_their_font_sizes.get(last_line_number_able_to_see)) / 20
 
-  streamlit.write(font_size)
+  streamlit.write('your eye number is',eye_number)
 
  else:
   streamlit.write('exceeding maximum line number')
+
+
