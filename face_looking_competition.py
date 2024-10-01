5f79b058d20c7b84532bf632d7530e5aa795920b
Viraj Sharma <virajsharma1501@gmail.com>
2024-10-01 22:11:15 +0530

    my app which judges who is most aesthetic in a picture

---
+++ face_looking_competition.py
@@ -0,0 +1,29 @@
+import cv2
+import cvlib
+import random
+import os
+import easygui
+
+image_file_name = easygui.fileopenbox('choose image file of people')
+
+if os.path.isfile(image_file_name):
+ image = cv2.imread(image_file_name)
+
+ faces, confidences = cvlib.detect_face(image)
+
+ if len(faces) > 0:
+  bounding_box = faces[confidences.index(max(confidences))]
+  
+  x1, y1, x2, y2 = bounding_box[0], bounding_box[1], bounding_box[2], bounding_box[3]
+
+  cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+  cv2.imshow('competition result', image)
+
+  cv2.waitKey(0)
+
+ else:
+  print('Error - no face found')
+
+else:
+ print('Error - file not found')
