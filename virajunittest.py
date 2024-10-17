13a5ddd6a60db2c3f000588d5c730315cef75e0a
Viraj Sharma <virajsharma1501@gmail.com>
2024-10-17 08:56:30 +0530

    adding feature in unittesting module which gives the list of testcases faild

--- virajunittest.py
+++ virajunittest.py
@@ -3,10 +3,20 @@ class TestCases:
   self.__function = function
   self.__total_testcases = 0
   self.__testcases_passed = 0
+  self.__faild_testcases_list = []
 
  def set_testcase(self, result, *args):
   self.__total_testcases += 1
-  self.__testcases_passed += self.__function(*args) == result
+
+  if not self.__function(*args) == result:
+   self.__faild_testcases_list.append({'result':result, 'arguments':args})
+
+  else:
+   self.__testcases_passed += 1
   
  def get_testcases_passed(self):
   return str(self.__testcases_passed) + '/' + str(self.__total_testcases)
+
+ def get_faild_testcases_list(self):
+  return self.__faild_testcases_list
+
--- virajunittest_module_test.py
+++ virajunittest_module_test.py
@@ -3,11 +3,39 @@ import virajunittest
 def sum_of_numbers(number1, number2):
  return number1 + number2
 
+def sum_of_numbers2(number1, number2):
+ return number1 + 1
+
+print('unittesting of function 1')
+
+print('\n')
+
 testcases = virajunittest.TestCases(sum_of_numbers)
 
 testcases.set_testcase(10, 8, 2)
 testcases.set_testcase(13, 7, 6)
 
 testcases_passed = testcases.get_testcases_passed()
+testcases_faild_list = testcases.get_faild_testcases_list()
 
-print(testcases_passed)
+print('testcases passed', testcases_passed)
+print('testcases faild list', testcases_faild_list)
+
+print('\n')
+
+print('unittesting of function2')
+
+print('\n')
+
+testcases = virajunittest.TestCases(sum_of_numbers2)
+
+testcases.set_testcase(9, 8, 1)
+testcases.set_testcase(6, 5, 1)
+testcases.set_testcase(10, 8, 2)
+testcases.set_testcase(11, 7, 4)
+
+testcases_passed_list = testcases.get_testcases_passed()
+testcases_faild_list = testcases.get_faild_testcases_list()
+
+print('testcases passed', testcases_passed_list)
+print('testcases faild list', testcases_faild_list)
