8d43ba3460b5d79b4e6a2b6f7fb1f70550e24180
Viraj Sharma <virajsharma1501@gmail.com>
2024-10-23 19:26:01 +0530

    updated message

--- streamlit/emotion_detection_streamlit.py
+++ streamlit/emotion_detection_streamlit.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import emotion_in_text_detector as emotion_detector
 
+st.write('<h1>this tool detects emotion in text with my simple made module called emotion_in_text_detector</h1>')
+
 text = st.text_input('Enter text')
 button = st.button('Detect Emotion')
 
