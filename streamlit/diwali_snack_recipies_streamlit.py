c2a202b11c192dc1b7a264ca9ef4557514561157
Viraj Sharma <virajsharma1501@gmail.com>
2024-11-02 12:19:11 +0530

    making UI better

--- streamlit/diwali_snack_recipies_streamlit.py
+++ streamlit/diwali_snack_recipies_streamlit.py
@@ -1,26 +1,49 @@
 import streamlit as st
 import diwali_snack_recipies
 
-for snack in diwali_snack_recipies.get_snack_options():
- st.write(snack)
+# Set the title and description
+st.title("🎉 Diwali Snack Recipes 🎉")
+st.subheader("Discover delicious and healthy recipes for your Diwali celebration!")
 
-st.write(' ')
+# Fetch and display the list of snack options
+snack_options = diwali_snack_recipies.get_snack_options()
+st.write("Select a snack from the menu below to get started:")
 
-snack_name = st.text_input('Enter snack name from above menu')
-healthy_or_tasty = st.text_input('snack should be healthy/tasty')
+# Display snack options in a two-column layout
+col1, col2 = st.columns(2)
+for i, snack in enumerate(snack_options):
+    if i % 2 == 0:
+        col1.write(f"✨ {snack}")
+    else:
+        col2.write(f"✨ {snack}")
 
-if st.button('Get Recipie'):
- recipie = diwali_snack_recipies.get_snack_recipies(snack_name, healthy = healthy_or_tasty == 'healthy', tasty = healthy_or_tasty == 'tasty')
+# Snack selection and preference inputs
+st.write(" ")
+snack_name = st.selectbox("Choose a snack:", options=snack_options)
+preference = st.radio("Would you prefer it to be:", options=["Healthy", "Tasty"])
 
- st.write('ingredients - ')
+# Button to get the recipe
+if st.button("Get Recipe"):
+    healthy = preference == "Healthy"
+    tasty = preference == "Tasty"
+    recipe = diwali_snack_recipies.get_snack_recipies(snack_name, healthy=healthy, tasty=tasty)
 
- for ingredient in recipie[0]:
-  st.write(ingredient)
+    # Display the recipe in organized sections
+    if recipe:
+        ingredients, instructions = recipe
 
- st.write(' ')
+        st.write(" ")
+        st.markdown("### 🥣 Ingredients:")
+        for ingredient in ingredients:
+            st.write(f"- {ingredient}")
 
- st.write('instructions - ')
+        st.write(" ")
+        st.markdown("### 🍲 Instructions:")
+        for step, instruction in enumerate(instructions, start=1):
+            st.write(f"{step}. {instruction}")
+    else:
+        st.error("Sorry, we couldn't find a recipe for that selection.")
 
- for instruction in recipie[1]:
-  st.write(instruction)
-
+# Footer
+st.write(" ")
+st.caption("Made with 💖 for Diwali. Enjoy your treats!")
