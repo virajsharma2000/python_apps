2dacf15c8e3233494680adb3840a2adf6be7b0aa
Viraj Sharma <virajsharma1501@gmail.com>
2025-01-07 13:19:21 +0530

    hiding api key using env

--- streamlit/written_work_summorizer.py
+++ streamlit/written_work_summorizer.py
@@ -1,10 +1,15 @@
 import requests
+from dotenv import load_dotenv
+import os
 
 def summorize_school_written_work(text):
+  load_dotenv()
+  api_key = os.getenv("API_KEY")
+  
   prompt = '{}\nsummorize text by extracting 4 points from the text of the answer, if the answer is too short, write the answer as it is, without changes, leave the answer as it is, then also do the same thing, and each point should be 1 line and put those points seperatly and mark each points as numbers and leave 1 line after each point, do not create para  or do not pack them into sentences or sentence from them  and do not summorize the questions, leave them as they are, but you have to write them'
   
   myobj = {"contents":[{"parts":[{"text":prompt.format(text)}]}]}
 
-  questions_and_answers = requests.post('https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyAIOM_Lav2P6Ip1BgZ8IgryqDIDDF1QeNY', json = myobj) 
+  questions_and_answers = requests.post('https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={api_key}', json = myobj) 
 
   return questions_and_answers.json()['candidates'][0]['content']['parts'][0]['text']
