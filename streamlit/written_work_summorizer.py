6ceaea6719615e3cf318cd4207b7ee609117ccfd
Viraj Sharma <virajsharma1501@gmail.com>
2025-01-07 13:21:03 +0530

    code fix

--- streamlit/written_work_summorizer.py
+++ streamlit/written_work_summorizer.py
@@ -1,9 +1,7 @@
 import requests
-from dotenv import load_dotenv
 import os
 
 def summorize_school_written_work(text):
-  load_dotenv()
   api_key = os.getenv("API_KEY")
   
   prompt = '{}\nsummorize text by extracting 4 points from the text of the answer, if the answer is too short, write the answer as it is, without changes, leave the answer as it is, then also do the same thing, and each point should be 1 line and put those points seperatly and mark each points as numbers and leave 1 line after each point, do not create para  or do not pack them into sentences or sentence from them  and do not summorize the questions, leave them as they are, but you have to write them'
