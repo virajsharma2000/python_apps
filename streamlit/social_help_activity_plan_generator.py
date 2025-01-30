7b716131e95975da87582be7e0fbf074c65489c0
viraj sharma <86789278+virajsharma2000@users.noreply.github.com>
2025-01-30 07:47:47 +0530

    Adding app title

--- streamlit/social_help_activity_plan_generator.py
+++ streamlit/social_help_activity_plan_generator.py
@@ -3,6 +3,8 @@ import requests
 import os
 import dotenv
 
+st.beta_set_page_config(page_title='CSR App - Viraj Sharma', page_icon = 'https://sharmaviraj.com/vicon.png', layout = 'wide', initial_sidebar_state = 'auto')
+
 st.title('CSR Application')
 
 st.subheader('Details of organisation')
