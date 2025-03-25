f43bd8be70295313ce2e5df1150d35c1902e78f0
Viraj Sharma <virajsharma1501@gmail.com>
2025-03-25 10:15:56 +0530

    changing confidence range and creating drone view code

--- obstacle_crossing_drone.py
+++ obstacle_crossing_drone.py
@@ -2,15 +2,18 @@ import cv2
 from ultralytics import YOLO, settings
 import json
 import logging
+import playsound
 
 logging.getLogger("ultralytics").setLevel(logging.ERROR)
 
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(2)
 yolo_model = YOLO('yolov8n.pt')
 
 while True:
  ret, frame = camera.read()
 
+ cv2.imshow('drone view', frame)
+ 
  objects = json.loads(yolo_model(frame)[0].to_json())
 
  confidences = []
@@ -18,8 +21,8 @@ while True:
  for object_ in objects:
   confidences.append(object_['confidence'])
  
- if confidences and max(confidences) >= 0.6 and max(confidences) <= 0.7:
-  print('go up, obstacle detected')
+ if confidences and max(confidences) > 0.7:
+  playsound.playsound('/home/viraj/Downloads/mixkit-shop-scanner-beeps-1073-[AudioTrimmer.com].wav')
 
  if cv2.waitKey(1) & 0xFF == ord('q'):
   break
