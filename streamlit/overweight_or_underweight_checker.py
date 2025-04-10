832d16821518f8b2b53b24223074d899b4262fd4
virajsharma2000 <virajsharma1501@gmail.com>
2025-04-10 13:13:43 +0530

    my app to determine whether a child is overweight or underweight using age and weight of th child

---
+++ streamlit/overweight_or_underweight_checker.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+def check_overweight_or_underweight(age, weight):
+ if age >= 1 and age <= 10:
+  appropriate_weight = 2 * age + 8
+
+ elif age >= 11 and age <= 18:
+  appropriate_weight = 3 * age
+
+ else:
+  st.error('age must be between 1 and 18')
+
+ return 'over weight' if weight > appropriate_weight else 'under weight' if weight < appropriate_weight else 'perfect weight'
+
+
+st.write('<h1>Check whether your child is having underweight, overweight or perfect weight within seconds!!</h1>', unsafe_allow_html = True)
+
+age = st.text_input('Enter your child\'s age')
+weight = st.text_input('Enter your child\'s weight')
+
+if st.button('Check'):
+ overweight_or_underweight = check_overweight_or_underweight(int(age), float(weight))
+ 
+ if overweight_or_underweight is not None:
+  st.success(f'your child is {overweight_or_underweight}')
+
+ 
