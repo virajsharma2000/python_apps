9ea2511e17dfd53ee723c69809e3f8ae7b33a8f6
virajsharma2000 <virajsharma1501@gmail.com>
2025-04-10 13:53:39 +0530

    changing requirements.txt

--- streamlit/overweight_or_underweight_checker.py
+++ streamlit/overweight_or_underweight_checker.py
@@ -4,13 +4,15 @@ def check_overweight_or_underweight(age, weight):
  if age >= 1 and age <= 10:
   appropriate_weight = 2 * age + 8
 
+  return 'over weight' if weight > appropriate_weight else 'under weight' if weight < appropriate_weight else 'perfect weight'
+
  elif age >= 11 and age <= 18:
   appropriate_weight = 3 * age
 
+  return 'over weight' if weight > appropriate_weight else 'under weight' if weight < appropriate_weight else 'perfect weight'
+
  else:
   st.error('age must be between 1 and 18')
-
- return 'over weight' if weight > appropriate_weight else 'under weight' if weight < appropriate_weight else 'perfect weight'
 
 
 st.write('<h1>Check whether your child is having underweight, overweight or perfect weight within seconds!!</h1>', unsafe_allow_html = True)
