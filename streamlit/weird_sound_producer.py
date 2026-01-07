7c7ccf1b45b04fb036a2503a9ca5034c18365b4e
virajsharma2000 <virajsharma1501@gmail.com>
2026-01-07 20:26:04 +0530

    my app to produce weird sounds and cause panic in places;

--- streamlit/weird_sound_producer.py
+++ streamlit/weird_sound_producer.py
@@ -6,7 +6,7 @@ st.title('MAKE WEIRD NOISES AND SCARE PEOPLE AND CAUSE PANICS EASIALLY!!! 😈')
 def make_weird_noises(duration):
  fs = 44100
 
- sound = np.array([[0.9] * 100 + [-0.9] * 100] * ((fs * duration) // 2 // 100)).flatten()
+ sound = np.array([[0.9] * 500 + [-0.9] * 500] * ((fs * duration) // 2 // 500)).flatten()
 
  st.audio(sound, sample_rate = fs)
 
