5bc0d14c55c0c2e4379c7e9b0243f7aa0459223e
virajsharma2000 <virajsharma1501@gmail.com>
2026-03-11 14:05:57 +0530

    changed the driver name

--- streamlit/youarejustashit.py
+++ streamlit/youarejustashit.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
-import pyttsx3
+from gtts import gTTS
 import os
 
 os.system('sudo apt install espeak')
@@ -8,11 +8,7 @@ st.header('Viraj says - YOU ARE JUST A SHIT 💩')
 
 if st.button('Click Me'):
  while True:
-  engine = pyttsx3.init()
-  engine.setProperty('rate', 120)
-  engine.say('you are just a shit')
-  engine.runAndWait()
-
+  tts = gTTS(text = 'you are just a shit', lang = 'en')
   components.html("""<script type="text/javascript">
             window.open('https://lordofpotty.streamlit.app');
         </script>""")
