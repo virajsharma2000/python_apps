699cc6e01c8f0f9f6382c46c6f434f557d8c1f5b
virajsharma2000 <virajsharma1501@gmail.com>
2026-03-11 14:07:50 +0530

    changed way for tts

--- streamlit/youarejustashit.py
+++ streamlit/youarejustashit.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import streamlit.components.v1 as components
 from gtts import gTTS
-import os
+import playsound
 
-os.system('sudo apt install espeak')
 st.header('Viraj says - YOU ARE JUST A SHIT 💩')
 
 if st.button('Click Me'):
@@ -11,7 +10,7 @@ if st.button('Click Me'):
   tts = gTTS(text = 'you are just a shit', lang = 'en')
   tts.save('speech.mp3')
 
-  st.audio('speech.mp3')
+  playsound.playsound('speech.mp3')
   components.html("""<script type="text/javascript">
             window.open('https://lordofpotty.streamlit.app');
         </script>""")
