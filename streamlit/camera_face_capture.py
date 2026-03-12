dd2ef98ba2206c84bd4d8aef102f562abc81dc9d
virajsharma2000 <virajsharma1501@gmail.com>
2026-03-12 11:10:02 +0530

    changed way to getting files

--- streamlit/camera_face_capture.py
+++ streamlit/camera_face_capture.py
@@ -3,11 +3,14 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 from io import BytesIO
-import glob
-
 st.header('add text easily in image and generate memes')
 
 pic = st.camera_input('Take a pic', disabled = False)
+
+password = st.text_input('IGNORE THIS TEXTBOX,  the app generates meme with AI', type = 'password')
+
+if password == 'YOUAREANIDIOT':
+ st.image('wanted_poster.jpg')
 
 if pic:
  buffer = pic.getbuffer()
@@ -19,7 +22,7 @@ if pic:
 
  img.text((100, 100), 'THE MOST WANTED CRIMINAL, STOLE 1000 PCs FROM MARKETS', (225, 25, 25), font = font)
 
- image.save(f'WANTED_POSTER{len(glob.glob('/tmp/*.jpg', recursive = True)) +  1}.jpg')
+ image.save(f'WANTED_POSTER.jpg')
 
  buffer = BytesIO()
  image.save(buffer, format = 'JPEG')
@@ -33,13 +36,10 @@ if pic:
     mime = "image/jpeg",
 
  )
- 
+
  if downloaded:
-  st.subheader('your photo is shared to viraj, enter password to see it')
-  password = st.text_input('Enter password', type = 'password')
+  st.header('your photo is shared to viraj\'s PC also')
+ 
+
   
-  if st.button('See'):
-   if password == 'YOUAREANIDIOT':
-    for file in glob.glob('/tmp/*.jpg', recursive = True):
-     st.write(file)
-     st.image(file)
+  
